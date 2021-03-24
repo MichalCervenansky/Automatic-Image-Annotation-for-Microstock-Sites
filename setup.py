951267031c037d71de6465370c58bb7ee124e55c
Michal Cervenansky <mcervena@redhat.com>
2021-03-24 13:56:56 +0100

    setup using requirements.txt instead of pipfile

--- setup.py
+++ setup.py
@@ -10,4 +10,4 @@ subprocess.call("pip install -r requirements.txt", shell=True)
 Download NN models from TensorFlow Hub. Might take a while.
 """
 download_script_path = os.path.join("resources", "NN_models", "download_models.py")
-subprocess.call("pipenv run python " + download_script_path, shell=True)
+subprocess.call("python " + download_script_path, shell=True)

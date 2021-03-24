52b97f24c852e3585cf9cb5aa8922af77989ea09
Michal Cervenansky <mcervena@redhat.com>
2021-03-24 13:04:57 +0100

    MUFFIN_annotate.py added

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ Firstly lets install pipenv and create virtual environment
 import subprocess
 import os
 
-subprocess.call(["pip", "install", "pipenv"])
-subprocess.call(["pipenv", "install"])
+subprocess.call(["pip", "install", "pipenv"], shell=True)
+subprocess.call(["pipenv", "install"], shell= True)
 
 """
 Download NN models from TensorFlow Hub. Might take a while.
 """
 download_script_path = os.path.join("resources", "NN_models", "download_models.py")
-subprocess.call(["pipenv", "run", "python", download_script_path])
+subprocess.call(["pipenv", "run", "python", download_script_path], shell=True)
